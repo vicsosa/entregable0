4223a102aec1189968082eda0045db02aa34971e
vicsosa <vicsosa.1610@gmail.com>
2022-08-20 00:35:31 -0300

    login en index y redirige a portada

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
+function erroreo(){
+    document.getElementById('incompleto').classList.add("show");
+    document.getElementById('clave1').style.borderColor = "red"
+    document.getElementById('email').style.borderColor = "red"
+}
 
+function exitoso(){
+    document.getElementById('exitoso').classList.add("show");
+}
+
+function ocultar(){
+    document.getElementById('incompleto').classList.remove("show");
+}
+
+function login(){
+    let usuario = document.getElementById('email').value;
+    let clave = document.getElementById('clave1').value;
+   if(usuario==="" || clave===""){ 
+    erroreo();
+   }else{
+    localStorage.setItem('dato', usuario);
+    exitoso(document.getElementById('incompleto').classList.remove("show"));
+    location.href="portada.html"
+}}
+
+document.addEventListener('DOMContentLoaded' ,() =>{
+    document.getElementById('ingresar').addEventListener('click' ,() =>{
+    login();
+    });
+})
--- js/portada.js
+++ js/portada.js
@@ -1,4 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(){
+
+    let usuario= localStorage.getItem('dato');
+    document.getElementById('usuario').innerHTML+=" " + usuario;
+
     document.getElementById("autos").addEventListener("click", function() {
         localStorage.setItem("catID", 101);
         window.location = "products.html"
