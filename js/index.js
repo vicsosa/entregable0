a5c2c5d5a2c3c5cb8c13f796023e03e0c676ee89
vicsosa <vicsosa.1610@gmail.com>
2022-08-25 21:37:24 -0300

    correcciones de entrega2; cambio1: indentificador de los productos con getItem catID, cambio2: innerHTML con 'productsArray.catName'

--- js/index.js
+++ js/index.js
@@ -1,26 +1,35 @@
-function erroreo(){
-    document.getElementById('incompleto').classList.add("show");
-    document.getElementById('clave1').style.borderColor = "red"
-    document.getElementById('email').style.borderColor = "red"
-}
-
-function exitoso(){
-    document.getElementById('exitoso').classList.add("show");
-}
-
-function ocultar(){
-    document.getElementById('incompleto').classList.remove("show");
+function redireccionar (){
+    location.href="portada.html"
 }
 
 function login(){
     let usuario = document.getElementById('email').value;
     let clave = document.getElementById('clave1').value;
    if(usuario==="" || clave===""){ 
-    erroreo();
+    document.getElementById('clave1').style.borderColor = "red"
+    document.getElementById('email').style.borderColor = "red"
+    Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Los datos no cumplen con los requisitos.',
+        confirmButtonText: 'Intentar  de nuevo',
+        confirmButtonColor: '#0087F7',
+        footer: '<p>¿Olvidaste tu contraseña?</p> ' + 
+        ' <a href=""> Recuperala aquí.</a>'
+      })
    }else{
     localStorage.setItem('dato', usuario);
-    exitoso(document.getElementById('incompleto').classList.remove("show"));
-    location.href="portada.html"
+    Swal.fire({
+        position: 'top',
+        icon: 'success',
+        title: 'Has inciado sesión',
+        confirmButtonText: '<a location.href="portada.html">¡Vamos a e-mercado!</a>',
+        confirmButtonColor: '#0087F7',
+        timer: '2500',
+        timerProgressBar: true,
+        backdrop: true, //cuando aparece el cartel, el fondo queda con sombrita
+      })
+      setTimeout("redireccionar()", 2000);
 }}
 
 document.addEventListener('DOMContentLoaded' ,() =>{
--- js/products.js
+++ js/products.js
@@ -1,25 +1,28 @@
-const direccion = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+const direccion = "https://japceibal.github.io/emercado-api/cats_products/";
 
 let productsArray = [];
+
+let categorías = localStorage.getItem('catID');
 
 function showAutos(array){
     let htmlAñadir = "";
 
     for(let i = 0; i < array.products.length; i++){ 
-        let auto = array.products[i];
-        htmlAñadir += `
+        let articulo = array.products[i];
+        htmlAñadir += 
+        `    
         <div class="list-group-item list-group-item-action">
             <div class="row">
                 <div class="col-3">
-                    <img src="` + auto.image + `" alt="product image" class="img-thumbnail">
+                    <img src="` + articulo.image + `" alt="product image" class="img-thumbnail">
                 </div>
                 <div class="col">
                     <div class="d-flex w-100 justify-content-between">
                         <div class="mb-1">
-                        <h4>`+ auto.name + " " + auto.currency + " " + auto.cost + `</h4> 
-                        <p> `+ auto.description +`</p> 
+                        <h4>`+ articulo.name + " " + articulo.currency + " " + articulo.cost + `</h4> 
+                        <p> `+ articulo.description +`</p> 
                         </div>
-                        <small class="text-muted">` + auto.soldCount + ` artículos</small> 
+                        <small class="text-muted">` + articulo.soldCount + ` artículos</small> 
                     </div>
                 </div>
             </div>
@@ -30,12 +33,14 @@ function showAutos(array){
 }
 
 document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(direccion).then(function(resultObj){
+    getJSONData(direccion + categorías + '.json').then(function(resultObj){
         if (resultObj.status === "ok")
         {
             productsArray = resultObj.data;
             showAutos(productsArray);
             console.log(productsArray);
+            document.getElementById('titulo').innerHTML+= `<h4>Productos</h4>
+            <h5>Verás aquí todos los productos de la categoría ` + productsArray.catName + ` </h5>` 
         }
     });
 });
